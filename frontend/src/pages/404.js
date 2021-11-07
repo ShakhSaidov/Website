fd61ebba30d8e97710515823519d4b30514e6196
Shakhzod Saidov <saidovs@lafayette.edu>
2021-11-07 15:47:18 +0000

    made the error page

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -4,7 +4,18 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>OOPS! The page does not seem to exist</h1>
+          <Link to="/" className="btn">
+            Get Back Home
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
